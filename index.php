ab7948927d5eafb125cea4d146cbd545af32954b
Iker Cayero <iker.1997.19@gmail.com>
2020-12-11 16:37:53 +0100

    Added landing page with assets

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TripCount</title>
+        <link rel="stylesheet" href="templates/landing.css">
+    </head>
+    <body>
+        <main>
+            <div id="containerLanding">
+                <div id="contenido">
+                    <p>
+                        Benvingut a l'aplicació TripCount, la millor aplicació per organitzar els teus viatges.
+                    </p>
+                    <div id="containerButtons">
+                        <form action="login.php" method="POST">
+                            <input type="submit" value="Login" class="button">
+                        </form>
+                        <form action="register.php" method="POST">
+                            <input type="submit" value="Registre" class="button">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
